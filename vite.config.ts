1d3e0e6c1898576d16659030bff09518f17d65be
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2025-11-16 23:07:47 -0800

    host true

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Expose to network
+    port: 5173,
+  },
   build: {
     outDir: 'dist',
     sourcemap: false,
